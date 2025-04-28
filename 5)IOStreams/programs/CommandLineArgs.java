d29335792213fdfa36c87e64ddeec351a2e86dd5
kotha-karthik <146633294+kotha-karthik@users.noreply.github.com>
2025-04-28 19:47:26 +0530

    Create CommandLineArgs.java

---
+++ 5)IOStreams/programs/CommandLineArgs.java
@@ -0,0 +1,11 @@
+class CommandLineArgs
+{
+	public static void main(String args[])
+	{
+		for(int i=0;i<args.length;i++)
+		{
+			System.out.println(args[i]);
+		}
+		System.console().readLine();
+	}
+}
