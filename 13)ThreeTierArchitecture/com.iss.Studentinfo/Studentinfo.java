3713c13a5db30124363af9083638d700884524ce
kotha-karthik <146633294+kotha-karthik@users.noreply.github.com>
2025-07-02 14:35:48 +0530

    Create Studentinfo.java

---
+++ 13)ThreeTierArchitecture/com.iss.Studentinfo/Studentinfo.java
@@ -0,0 +1,27 @@
+package com.iss.models;
+
+public class StudentInfo {
+
+	private int id;
+	private String name;
+	private double fee;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getFee() {
+		return fee;
+	}
+	public void setFee(double fee) {
+		this.fee = fee;
+	}
+}
