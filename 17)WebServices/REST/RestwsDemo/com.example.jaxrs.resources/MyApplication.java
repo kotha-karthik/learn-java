7c69eb0726b16204c1dbc6a2fa0c53845565114a
kotha-karthik <146633294+kotha-karthik@users.noreply.github.com>
2025-08-26 22:25:54 +0530

    Create MyApplication.java

---
+++ 17)WebServices/REST/RestwsDemo/com.example.jaxrs.resources/MyApplication.java
@@ -0,0 +1,9 @@
+package com.example.jaxrs.resources;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class MyApplication extends Application{
+
+}
